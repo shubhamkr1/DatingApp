a9798654ab4b07fca519e21ec3dde0d05ca9c99f
Shubham Kumar <shubhamkumar007@gmail.com>
2019-03-24 21:03:28 -0400

    register user methods

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string username, string password)
+        {
+            // validate request
+            // username nt case sensitive
+            username = username.ToLower();
+
+            if (await _repo.UserExists(username))
+                return BadRequest("Username already exist");
+
+            var userToCreate = new User
+            {
+                Username = username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, password);
+            // change the return later
+            return StatusCode(201);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -28,6 +28,8 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
             services.AddCors(); // allows cross origin requests
+            //scoped suitable for out iauth async tasks 
+            services.AddScoped<IAuthRepository, AuthRepository>(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
